dc779eedd1705da718edcb8bbd492d19f191af1e
Kieran Gee <kieran.lost.the.game@gmail.com>
2022-03-31 21:27:15 +0800

    Split out qmk and via

---
+++ qmk/annepro2/keymaps/kism/keymap.c
@@ -0,0 +1,201 @@
+//mkdir annepro-qmk/keyboards/annepro2/keymaps/kism/
+//ln qmk-keymap-annepro2/keymap.c annepro-qmk/keyboards/annepro2/keymaps/kism/keymap.c
+
+#include <stdint.h>
+#include "annepro2.h"
+#include "qmk_ap2_led.h"
+
+enum anne_pro_layers {
+  _BASE_LAYER,
+  _FN1_LAYER,
+  _FN2_LAYER,
+  _FN3_LAYER,
+  _FN4_LAYER,
+  _FN5_LAYER,
+  _FN6_LAYER,
+};
+
+/*
+* Layer _BASE_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* | ~   |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | esc     |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  FN1  |  FN2  |  Alt  | Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+* Layer TAP in _BASE_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
+* |-----------------------------------------------------------------------------------------+
+* |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
+* |-----------------------------------------------------------------------------------------+
+* |         |     |     |     |     |     |     |     |     |     |     |     |             |
+* |-----------------------------------------------------------------------------------------+
+* |            |     |     |     |     |     |     |     |     |     |     |       UP       |
+* |-----------------------------------------------------------------------------------------+
+* |       |       |       |                                 |       |  LEFT | DOWN  | RIGHT |
+* \-----------------------------------------------------------------------------------------/
+*/
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_BASE_LAYER] = KEYMAP( /* Base */
+   KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+   KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+   KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),   
+   KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(_FN1_LAYER), LT(_FN2_LAYER,KC_LEFT),RCTL_T(KC_DOWN), RCTL_T(KC_RGHT)
+),
+/*
+* Layer _FN1_LAYER | F1 - F12
+* ,-----------------------------------------------------------------------------------------.
+* |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | Caps    |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  FN1  |  FN3  | Alt  |  Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+[_FN1_LAYER] = KEYMAP( /* Base */
+   KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+   KC_TRNS, KC_TRNS, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
+   KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN1_LAYER), MO(_FN3_LAYER), KC_TRNS, KC_TRNS
+),
+/*
+* Layer _FN2_LAYER | Media Keys
+* ,-----------------------------------------------------------------------------------------.
+* | ~ |MyCom|HTTP|CALC |MPLAY |PREV |NEXT |P/PAUSE|STOP |MUTE |VOL- |VOL+ | F12 |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | Esc     |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      | BT1 | BT2 | BT3 | BT4 |LEDON|LEDOFF| m  |  ,  |  .  |  /  |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN3  |  FN2  | Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+[_FN2_LAYER] = KEYMAP( /* Base */
+   KC_TRNS, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, KC_AP_LED_ON, KC_AP_LED_OFF, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN3_LAYER), MO(_FN2_LAYER), KC_TRNS, KC_TRNS
+),
+/*
+* Layer _FN3_LAYER | F13-24
+* ,-----------------------------------------------------------------------------------------.
+* |  `  | F13 | F14 | F15 | F16 | F17 | F18 | F19 | F20 | F21 | F22 | F23 | F24 |  DELETE   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | Esc     |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  Alt  |       |       | Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+[_FN3_LAYER] = KEYMAP(
+   KC_TRNS, KC_F13, KC_F14, KC_F15, KC_F16, KC_F17, KC_F18, KC_F19, KC_F20, KC_F21, KC_F22, KC_F23, KC_F24, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN3_LAYER), MO(_FN3_LAYER), MO(_FN4_LAYER), MO(_FN5_LAYER)
+
+),
+/*
+* Layer _FN4_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* | `   | K1  | K2  | K3  | K4  | K5  | K6  | K7  | K8  | K9  | K0  | K-  | K=  |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | Numlock |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  FN6  |       |       | FN5   |
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+[_FN4_LAYER] = KEYMAP(
+   KC_TRNS, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_MINUS, KC_KP_EQUAL, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_NUMLOCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN3_LAYER), MO(_FN3_LAYER), MO(_FN4_LAYER), MO(_FN6_LAYER)
+
+),
+/*
+* Layer _FN4_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* | `   | K1  | K2  | K3  | K4  | K5  | K6  | K7  | K8  | K9  | K0  | K-  | K=  |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |   \    |
+* |-----------------------------------------------------------------------------------------+
+* | Numlock |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  FN4  |       |       | FN6   |
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+[_FN5_LAYER] = KEYMAP(
+   KC_TRNS, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_MINUS, KC_KP_EQUAL, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_NUMLOCK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN3_LAYER), MO(_FN3_LAYER), MO(_FN6_LAYER), MO(_FN4_LAYER)
+
+),
+/*
+* Layer _FN5_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* | `   | K1  | K2  | K3  | K4  | K5  | K6  | K7  | K8  | K9  | K0  | K-  | K=  |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  P  |  E  |  P  |  S  |  I  |    |  P  |  E  |  P  |  S  |  I  |     |   \     |
+* |-----------------------------------------------------------------------------------------+
+* | Numlock |  P  |  E  |  P  |  S  |  I  |    |  P  |  E  |  P  |  S  |  I   |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  P  |  E  |  P  |  S  |  I  |    |  P  |  E  |  P  |  S   |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  L1   |  Alt  |               space             |  Alt  |       |       | Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+*
+*/
+[_FN6_LAYER] = KEYMAP(
+   KC_TRNS, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_MINUS, KC_KP_EQUAL, KC_TRNS,
+   KC_TRNS, KC_P, KC_E, KC_P, KC_S, KC_I, KC_SPC, KC_P, KC_E, KC_P, KC_S, KC_I, KC_SPC, KC_TRNS,
+   KC_NUMLOCK, KC_P, KC_E, KC_P, KC_S, KC_I, KC_SPC, KC_P, KC_E, KC_P, KC_S, KC_I, KC_TRNS,
+   KC_TRNS, KC_P, KC_E, KC_P, KC_S, KC_I, KC_SPC, KC_P, KC_E, KC_P, KC_S, KC_TRNS,
+   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(_FN3_LAYER), MO(_FN3_LAYER), MO(_FN6_LAYER), MO(_FN6_LAYER)
+
+),
+};
+const uint16_t keymaps_size = sizeof(keymaps);
+
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+}
+
+layer_state_t layer_state_set_user(layer_state_t layer) {
+  return layer;
+}
